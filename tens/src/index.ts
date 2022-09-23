55095a5aed034ebc9dc2ab8f479628d44138abb2
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-24 02:30:38 +0900

    メモリを確認

--- tens/src/index.ts
+++ tens/src/index.ts
@@ -1,9 +1,28 @@
 import * as tf from '@tensorflow/tfjs';
 
-console.log('Start');
-const data = [0, 1, 2, 3, 4, 5, 6];
-const tensor1 = tf.tensor1d(data);
-const tensorAgain = tf.tensor(data);
-console.log(tensor1);
-console.log(tensorAgain);
-console.log('End');
+console.log('start');
+console.log(tf.memory().numTensors);
+console.log(tf.memory().numBytes);
+
+let keeper, chaser, seeker, beater;
+
+tf.tidy(() => {
+  keeper = tf.tensor([1, 2, 3]);
+  chaser = tf.tensor([1, 2, 3]);
+  seeker = tf.tensor([1, 2, 3]);
+  beater = tf.tensor([1, 2, 3]);
+
+  console.log('inside', tf.memory().numTensors);
+
+  tf.keep(keeper);
+  return chaser;
+});
+
+console.log('after', tf.memory().numTensors);
+console.log(tf.memory().numBytes);
+
+keeper.dispose();
+chaser.dispose();
+
+console.log('end', tf.memory().numTensors);
+console.log(tf.memory().numBytes);
