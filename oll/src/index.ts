e8468f9c140f4380be57829d83e86910f20eff0f
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-23 16:40:57 +0900

    change

--- oll/src/index.ts
+++ oll/src/index.ts
@@ -1,6 +1,25 @@
-// import * as tf from '@tensorflow/tfjs-node';
-const tf = require('@tensorflow/tfjs-node');
+import * as tf from '@tensorflow/tfjs-node';
 
-console.log(tf.version.tfjs);
-console.log(tf.version.tfjs);
-console.log('Finish');
+// console.log(tf.version.tfjs);
+// console.log('Finish');
+
+import * as toxicity from '@tensorflow-models/toxicity';
+
+const threshold = 0.5;
+const label = [
+  // 'identity_attack',
+  // 'insult',
+  // 'obscene',
+  // 'severe_toxicity',
+  // 'sexual_explicit',
+  // 'threat',
+  // 'toxicity',
+];
+
+toxicity.load(threshold, label).then((model) => {
+  const sentences = ['You are a poopy head!', 'I like turtles', 'Shut up!'];
+
+  model.classify(sentences).then((predictions) => {
+    console.log(JSON.stringify(predictions, null), 2);
+  });
+});
