aded20787cf79c9e2787033577a731ed28f64cf7
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-24 22:27:25 +0900

    ユーザーの興味を計算

--- tens/src/index.ts
+++ tens/src/index.ts
@@ -1,16 +1,52 @@
 import * as tf from '@tensorflow/tfjs';
 
-const snap = tf.tensor([1, 2, 3]);
-const crackle = tf.tensor([3.141592654]);
-const pop = tf.tensor([
-  [1, 2, 3],
-  [4, 5, 6],
-]);
+tf.tidy(() => {
+  const users = ['Gant', 'Todd', 'Jed', 'Justin'];
+  const bands = [
+    'Nirvana',
+    'Nine Inch Nails',
+    'Backstreet Boys',
+    'N Sync',
+    'Night Club',
+    'Apashe',
+    'STP',
+  ];
+  const features = [
+    'Grunge',
+    'Rock',
+    'Industrial',
+    'Boy Band',
+    'Dance',
+    'Techno',
+  ];
 
-console.log(snap);
-crackle.print();
+  const user_votes = tf.tensor([
+    [10, 9, 1, 1, 8, 7, 8],
+    [6, 8, 2, 2, 0, 10, 0],
+    [0, 2, 10, 9, 3, 7, 0],
+    [7, 4, 2, 3, 6, 5, 5],
+  ]);
 
-console.log('Welcome Back Array', pop.arraySync());
-console.log('Welcome Back Typed!', pop.dataSync());
+  const band_feats = tf.tensor([
+    [1, 1, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 1],
+    [0, 0, 1, 0, 0, 1],
+    [1, 1, 0, 0, 0, 0],
+  ]);
 
-tf.dispose([snap, crackle, pop]);
+  const user_feats = tf.matMul(user_votes, band_feats);
+  user_feats.print();
+
+  const top_user_features = tf.topk(user_feats, features.length);
+  const top_genres = top_user_features.indices.arraySync();
+
+  users.map((u, i) => {
+    const rankedCategories = top_genres[i].map(
+      (v: string | number): string => features[v]
+    );
+    console.log(u, rankedCategories);
+  });
+});
