dc801f0d5ce39dc43ef4d73c8159aa413cedb9da
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-25 16:17:11 +0900

    ブラウザ上の画像をtensorに変換してshapeを出力

--- im/src/index.ts
+++ im/src/index.ts
@@ -1,26 +1,11 @@
 import * as tf from '@tensorflow/tfjs';
-import { Tensor3D } from '@tensorflow/tfjs-core';
+import { Tensor3D } from '@tensorflow/tfjs';
 
 tf.tidy(() => {
-  const bigMess: Tensor3D = tf.randomUniform([100, 200, 3]);
+  const gantImage: HTMLElement = document.getElementById('gant')!;
+  const gantTensor: Tensor3D = tf.browser.fromPixels(
+    gantImage as HTMLImageElement
+  );
 
-  // const tensorOne = tf.fill([100, 100, 1], 0.2);
-  // const tensorTwo = tf.fill([100, 100, 1], 0.5);
-  // const tensorThree = tf.fill([100, 100, 1], 0.8);
-
-  // const tensorOne = tf.fill([10, 20, 1], 0.7);
-  // const tensorTwo = tf.fill([10, 20, 1], 0.1);
-  // const tensorThree = tf.fill([10, 20, 1], 0.7);
-
-  // const bigMess = tf.concat([tensorOne, tensorTwo, tensorThree], 2);
-
-  bigMess.print();
-  console.log(bigMess.shape);
-  const myCanvas = document.getElementById('randomness');
-  tf.browser
-    .toPixels(bigMess as Tensor3D, myCanvas as HTMLCanvasElement)
-    .then(() => {
-      bigMess.dispose();
-      console.log('Make sure we cleaned up', tf.memory().numTensors);
-    });
+  console.log(`Gant tensor shape: ${gantTensor.shape}`);
 });
