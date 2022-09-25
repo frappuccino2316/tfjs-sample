cd80f902c33071e106a5e4afe0ad2a94b4ebf479
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-25 17:21:49 +0900

    画像をtensorに変換

---
+++ note/src/index.ts
@@ -0,0 +1,17 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as tf from '@tensorflow/tfjs-node';
+import { Tensor3D, Tensor4D } from '@tensorflow/tfjs-node';
+
+const FILE_PATH = 'files';
+const cakeImagePath = path.join(FILE_PATH, 'cake.jpg');
+const cakeImage = fs.readFileSync(cakeImagePath);
+
+tf.tidy(() => {
+  const cakeTensor: Tensor3D | Tensor4D = tf.node.decodeImage(cakeImage);
+  console.log(`cakeTensor shape is ${cakeTensor.shape}`);
+  cakeTensor.print();
+
+  const cakeBWTensor: Tensor3D | Tensor4D = tf.node.decodeImage(cakeImage, 1);
+  console.log(`cakeBWTensor shape is ${cakeBWTensor.shape}`);
+});
