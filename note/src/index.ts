afaf00e1e2b107a7e5e173cf8f339195aca83750
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-25 17:32:38 +0900

    gifを読み込み

--- note/src/index.ts
+++ note/src/index.ts
@@ -6,16 +6,17 @@ import * as tf from '@tensorflow/tfjs-node';
 import { Tensor3D, Tensor4D } from '@tensorflow/tfjs-node';
 
 const FILE_PATH = 'files';
-const cakeImagePath = path.join(FILE_PATH, 'cake.jpg');
-const cakeImage = fs.readFileSync(cakeImagePath);
+const gantcakeImagePath = path.join(FILE_PATH, 'gantcake.gif');
+const gantcakeImage = fs.readFileSync(gantcakeImagePath);
 
 tf.tidy(() => {
-  const cakeTensor: Tensor3D | Tensor4D = tf.node.decodeImage(cakeImage);
-  console.log(`cakeTensor shape is ${cakeTensor.shape}`);
-
-  const cakeBWTensor: Tensor3D | Tensor4D = tf.node.decodeImage(cakeImage, 1);
-  console.log(`cakeBWTensor shape is ${cakeBWTensor.shape}`);
-  cakeBWTensor.print();
+  const gantcakeTensor: Tensor3D | Tensor4D = tf.node.decodeImage(
+    gantcakeImage,
+    3,
+    'int32',
+    true
+  );
+  console.log(`gantcakeTensor shape is ${gantcakeTensor.shape}`);
 });
 
 console.timeEnd('note');
