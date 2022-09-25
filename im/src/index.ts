ff8bcb25f8aec74b006b300617c9dbf448c1cadf
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-25 16:03:37 +0900

    高さ・幅を変えてみる

--- im/src/index.ts
+++ im/src/index.ts
@@ -2,17 +2,17 @@ import * as tf from '@tensorflow/tfjs';
 import { Tensor3D } from '@tensorflow/tfjs-core';
 
 tf.tidy(() => {
-  // const bigMess: Tensor3D = tf.randomUniform([100, 100, 3]);
+  const bigMess: Tensor3D = tf.randomUniform([100, 200, 3]);
 
   // const tensorOne = tf.fill([100, 100, 1], 0.2);
   // const tensorTwo = tf.fill([100, 100, 1], 0.5);
   // const tensorThree = tf.fill([100, 100, 1], 0.8);
 
-  const tensorOne = tf.fill([100, 100, 1], 0.7);
-  const tensorTwo = tf.fill([100, 100, 1], 0.1);
-  const tensorThree = tf.fill([100, 100, 1], 0.7);
+  // const tensorOne = tf.fill([10, 20, 1], 0.7);
+  // const tensorTwo = tf.fill([10, 20, 1], 0.1);
+  // const tensorThree = tf.fill([10, 20, 1], 0.7);
 
-  const bigMess = tf.concat([tensorOne, tensorTwo, tensorThree], 2);
+  // const bigMess = tf.concat([tensorOne, tensorTwo, tensorThree], 2);
 
   bigMess.print();
   console.log(bigMess.shape);
