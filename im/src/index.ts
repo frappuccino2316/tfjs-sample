c8b5d7a462b0670f4b673c1da1ff1b02756e68e1
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-25 12:36:59 +0900

    色ごとにtensorを作成してconcatする

--- im/src/index.ts
+++ im/src/index.ts
@@ -2,10 +2,25 @@ import * as tf from '@tensorflow/tfjs';
 import { Tensor3D } from '@tensorflow/tfjs-core';
 
 tf.tidy(() => {
-  const bigMess: Tensor3D = tf.randomUniform([400, 400, 3]);
+  // const bigMess: Tensor3D = tf.randomUniform([100, 100, 3]);
+
+  // const tensorOne = tf.fill([100, 100, 1], 0.2);
+  // const tensorTwo = tf.fill([100, 100, 1], 0.5);
+  // const tensorThree = tf.fill([100, 100, 1], 0.8);
+
+  const tensorOne = tf.fill([100, 100, 1], 0.7);
+  const tensorTwo = tf.fill([100, 100, 1], 0.1);
+  const tensorThree = tf.fill([100, 100, 1], 0.7);
+
+  const bigMess = tf.concat([tensorOne, tensorTwo, tensorThree], 2);
+
+  bigMess.print();
+  console.log(bigMess.shape);
   const myCanvas = document.getElementById('randomness');
-  tf.browser.toPixels(bigMess, myCanvas as HTMLCanvasElement).then(() => {
-    bigMess.dispose();
-    console.log('Make sure we cleaned up', tf.memory().numTensors);
-  });
+  tf.browser
+    .toPixels(bigMess as Tensor3D, myCanvas as HTMLCanvasElement)
+    .then(() => {
+      bigMess.dispose();
+      console.log('Make sure we cleaned up', tf.memory().numTensors);
+    });
 });
