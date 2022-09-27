71cf9b886da5dca1822617a44fcdf2589581d590
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-27 22:48:22 +0900

    ready

---
+++ incept/src/index.ts
@@ -0,0 +1,33 @@
+import * as tf from '@tensorflow/tfjs';
+import { Rank, Tensor } from '@tensorflow/tfjs';
+import { INCEPTION_CLASSES } from './labels.js';
+
+tf.ready().then(() => {
+  const modelPath =
+    'https://tfhub.dev/google/tfjs-model/imagenet/inception_v3/classification/3/default/1';
+  tf.tidy(() => {
+    tf.loadGraphModel(modelPath, { fromTFHub: true }).then((model) => {
+      const mysteryImage = document.getElementById('mystery');
+      const myTensor = tf.browser.fromPixels(mysteryImage as HTMLImageElement);
+      // Inception v3 expects an image resized to 299x299
+      const readyfied = tf.image
+        .resizeBilinear(myTensor, [299, 299], true)
+        .div(255)
+        .reshape([1, 299, 299, 3]);
+
+      const result = model.predict(readyfied) as Tensor<Rank>;
+      result.print(); // useless
+
+      const { values, indices } = tf.topk(result, 3);
+      indices.print();
+
+      // Let's hear those winners
+      const winners = indices.dataSync();
+      console.log(`
+              🥇 First place ${INCEPTION_CLASSES[winners[0]]},
+              🥈 Second place ${INCEPTION_CLASSES[winners[1]]},
+              🥉 Third place ${INCEPTION_CLASSES[winners[2]]}
+            `);
+    });
+  });
+});
