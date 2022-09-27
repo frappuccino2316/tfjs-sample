52bd66fa78b6ce7476b73d1708b5cd053d01ffe8
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-27 22:55:39 +0900

    display

--- incept/src/index.ts
+++ incept/src/index.ts
@@ -1,12 +1,16 @@
 import * as tf from '@tensorflow/tfjs';
 import { Rank, Tensor } from '@tensorflow/tfjs';
-import { INCEPTION_CLASSES } from './labels.js';
+import { INCEPTION_CLASSES } from './labels';
 
+console.log('start');
 tf.ready().then(() => {
+  console.log('ready');
   const modelPath =
     'https://tfhub.dev/google/tfjs-model/imagenet/inception_v3/classification/3/default/1';
   tf.tidy(() => {
+    console.log('tidy');
     tf.loadGraphModel(modelPath, { fromTFHub: true }).then((model) => {
+      console.log('loaded');
       const mysteryImage = document.getElementById('mystery');
       const myTensor = tf.browser.fromPixels(mysteryImage as HTMLImageElement);
       // Inception v3 expects an image resized to 299x299
@@ -15,7 +19,9 @@ tf.ready().then(() => {
         .div(255)
         .reshape([1, 299, 299, 3]);
 
+      console.log('predict start');
       const result = model.predict(readyfied) as Tensor<Rank>;
+      console.log('predict end');
       result.print(); // useless
 
       const { values, indices } = tf.topk(result, 3);
