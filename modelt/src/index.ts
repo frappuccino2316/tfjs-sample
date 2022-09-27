65fe246810d915e2112f7f943843e1bd79703e25
frappuccino2316 <frappuccino2316@gmail.com>
2022-09-27 22:18:31 +0900

    ready

--- modelt/src/index.ts
+++ modelt/src/index.ts
@@ -2,9 +2,11 @@ import * as tf from '@tensorflow/tfjs';
 import { Rank, Tensor } from '@tensorflow/tfjs';
 
 tf.ready().then(() => {
-  const modelPath = 'model/ttt_model.json';
+  const modelPath = '../model/ttt_model.json';
   tf.tidy(() => {
+    console.log('tidy');
     tf.loadLayersModel(modelPath).then((model) => {
+      console.log('loaded');
       // Three board states
       const emptyBoard = tf.zeros([9]);
       const betterBlockMe = tf.tensor([-1, 0, 0, 1, 1, -1, 0, 0, -1]);
@@ -12,7 +14,9 @@ tf.ready().then(() => {
 
       // Stack states into a shape [3, 9]
       const matches = tf.stack([emptyBoard, betterBlockMe, goForTheKill]);
+      console.log('stack');
       const result = model.predict(matches);
+      console.log('predicted');
       // Log the results
       (result as Tensor<Rank>).reshape([3, 3, 3]).print();
     });
